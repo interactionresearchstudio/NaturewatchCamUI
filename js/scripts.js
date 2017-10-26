0908f0e78ec00dba08e259456b2334cee9aa9dfe
Mike Vanis <hello@mikevanis.com>
2017-10-26 15:21:43 +0100

    embedded mjpg

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 var controllingMin = 1;
 $(document).ready(function() {
     // Check if camera feed is available.
-    /*
     $.get("python/feed.mjpg")
         .done(function() {
-            $("#feed").append('<img src="python/feed.mjpg">');
+            console.log("Camera feed is available.");
         }).fail(function() {
             $("#feed").append('<img src="assets/unavailable.jpg">');
         });
-    */
 
     $("#min-controls").hide();
     $("#max-controls").hide();
